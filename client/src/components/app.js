c8589730917fcc43c7df8b45a928754ccf6cb9db
Julie Wang <julie_wang2@brown.edu>
2024-08-08 21:12:05 -0400

    started creating basic skeleton

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+import Event from "./Event";
 
 export default class extends React.Component {
-
-    render() {
-        return (
-          <div>
-            <h1>Your JS-Scaffold is up and running</h1>
-            <div>Now go out and make something!</div>
-          </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Your JS-Scaffold is up and running</h1>
+        <Event
+          eventName="LigerBots DCMP"
+          eventDate="8/8/2024"
+          eventDescription="Pack lightly"
+          eventRides={["Hello"]}
+        />
+      </div>
+    );
+  }
 }
